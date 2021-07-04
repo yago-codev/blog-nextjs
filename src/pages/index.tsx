f38d419da3ac70cdd19115211bad7afa32f1558b
yago-codev <yagomilano92@gmail.com>
2021-07-04 02:28:00 -0300

    estilos globais com css/sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Olá <span>Dev!</span></h1>
+    <h1>
+      Olá
+      <span>
+        {` `}Dev!
+      </span>
+    </h1>
   )
 }
