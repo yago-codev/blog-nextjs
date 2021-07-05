04854f403604151ef979b33665132fa9f2c669d5
yago-codev <yagomilano92@gmail.com>
2021-07-04 22:05:48 -0300

    client side rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react'
+import { baseUrl } from '../services'
+
 import styles from '../styles/home.module.scss'
 
+interface IPost {
+  id: string
+  title: string
+}
+
 export default function Home() {
+  const [posts, setPosts] = useState<IPost[]>([])
+
+  useEffect(() => {
+    fetch(baseUrl)
+      .then(response => {
+        response.json()
+          .then(data => setPosts(data))
+      })
+  }, [])
+
   return (
-    <h1>
-      Olá
-      <span>
-        {` `}Dev!
-      </span>
-    </h1>
+    <>
+      <h1>
+        Posts
+      </h1>
+      <ul>
+        {posts.map(({ id, title }) => (
+          <li key={id}>
+            {title}
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
