3ea6e28e465cbe08858c66d0bd604592c598a00a
yago-codev <yagomilano92@gmail.com>
2021-07-06 18:33:39 -0300

    lazy load component: carregando componentes sob demanda

--- src/pages/calculation.tsx
+++ src/pages/calculation.tsx
@@ -1,4 +1,18 @@
+/* eslint-disable */
+import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const ModalCustom = dynamic(() => import('../components/ModalCustom').then((mod) => mod.ModalCustom).finally(),
+  { loading: () => <p>Loading...</p>, ssr: false },
+);
+
 export default function Calculation() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+
+  const handleCloseModal = () => setIsModalOpen(false);
+
   const handleSum = async () => {
     const { sum } = (await import('../libs/calc')).default;
     alert(sum(3, 5));
@@ -7,7 +21,21 @@ export default function Calculation() {
   return (
     <div>
       <h1>Cálculo</h1>
-      <button onClick={handleSum}>Somar</button>
+      <button
+        onClick={() => {
+          handleSum();
+          handleOpenModal();
+        }}
+      >
+        Somar
+      </button>
+      {isModalOpen && (
+        <ModalCustom
+          isOpen={isModalOpen}
+          onRequestClose={handleCloseModal}
+          title="Modal"
+        />
+      )}
     </div>
   );
 }
