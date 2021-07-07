f6a1dfa541acc3b981ecc6bd848891acd5b6e1b6
yago-codev <yagomilano92@gmail.com>
2021-07-06 22:52:38 -0300

    criando a estrutura semântica da página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,23 @@
-import { GetServerSideProps } from 'next';
-// import { useEffect, useState } from 'react';
-import { api } from '../services';
-
-// import styles from '../styles/home.module.scss';
 import { SEO } from '../components/SEO';
 
-export interface IPost {
-  id: number;
-  title: string;
-}
-
-export interface IHomeProps {
-  posts: IPost[];
-}
-
-export default function Home({ posts }: IHomeProps) {
-  // const { postsUrl } = api;
-  // const [posts, setPosts] = useState<IPost[]>([])
-
-  // useEffect(() => {
-  //   fetch(postsUrl)
-  //     .then(response => {
-  //       response.json()
-  //         .then(data => setPosts(data))
-  //     })
-  // }, [])
-
+export default function Home() {
   return (
-    <div>
-      <SEO title="Home" />
-      <h1>Posts</h1>
-      <ul>
-        {posts.map(({ id, title }) => (
-          <li key={id}>{title}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title="Dev News!" excludeTitleSuffix />
+      <main>
+        <section>
+          <span>Olá Dev!</span>
+          <h1>
+            Bem-vindo(a)s ao <br />
+            <span>Dev</span> News!
+          </h1>
+          <p>
+            Um blog com conteúdos extremamente <br />
+            <span>relevantes para o seu aprendizado.</span>
+          </p>
+        </section>
+        <img src="/home.svg" alt="Imagem da página Home" />
+      </main>
+    </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
-  const { postsUrl } = api;
-
-  const response = await fetch(postsUrl);
-
-  const posts = await response.json();
-
-  return {
-    props: {
-      posts
-    }
-  };
-};
