3d01982ed578780e2b9e61cf41e1ef6f8463b0fa
yago-codev <yagomilano92@gmail.com>
2021-07-07 21:45:26 -0300

    utilizando a função de conexão com o prismic para fazer a primeira busca de addos

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
+import Prismic from '@prismicio/client';
 
 import { SEO } from '../../components/SEO';
-
-import { api } from '../../services/index';
+import { getPrismicClient } from '../../services/prismic';
 
 import styles from './posts.module.scss';
 
@@ -36,6 +36,17 @@ export default function Posts() {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content']
+    }
+  );
+
+  console.log(response);
+
   return {
     props: {},
     revalidate: 60 * 60 * 12 // 12 horas
