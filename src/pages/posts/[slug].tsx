37a051e76efd8e1da2bad66043061b5cb611521c
yago-codev <yagomilano92@gmail.com>
2021-07-09 19:01:27 -0300

    buscando e renderizando dados de um único post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,26 +1,26 @@
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { useRouter } from 'next/router';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { getPrismicClient } from '../../services/prismic';
 
 import { SEO } from '../../components/SEO';
 
-import { api } from '../../services/index';
-
 import styles from './post.module.scss';
 
-interface ICommentProps {
-  id: number;
-  body: string;
+interface IPostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  };
 }
 
-interface ICommentsProps {
-  comments: ICommentProps[];
-}
-
-export default function Post({ comments }: ICommentsProps) {
-  const {
-    query: { id },
-    isFallback
-  } = useRouter();
+export default function Post({
+  post: { title, content, updatedAt }
+}: IPostProps) {
+  const { isFallback } = useRouter();
 
   return isFallback ? (
     <p>Loading...</p>
@@ -29,9 +29,16 @@ export default function Post({ comments }: ICommentsProps) {
       <SEO title="Post" />
       <main className={styles.container}>
         <article className={styles.post}>
-          <h1>Titulo</h1>
-          <time>Data</time>
-          <div className={styles.content}>Conteudo</div>
+          <h1>{title}</h1>
+          <time>{updatedAt}</time>
+          {/* Através do "dangerouslySetInnerHTML iremos fazer a introdução do
+          valor que será introduzido no post. Esse valor precisa ser introduzido através
+          dessa propriedade porque estamos formatando diversos tipos conteúdos que
+          estão vindo do Prismic" */}
+          <div
+            className={styles.content}
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
         </article>
       </main>
     </>
@@ -57,8 +64,32 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async context => {
+  const { slug } = context.params;
+
+  // cria a conexão com o Prismic
+  const prismic = getPrismicClient();
+
+  // fazer query para buscar dados no Prismic
+  const response = await prismic.getByUID('post', String(slug), {});
+
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    content: RichText.asText(response.data.content),
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+      'pt-BR',
+      {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }
+    )
+  };
+
   return {
-    props: {},
+    props: {
+      post
+    },
     revalidate: 60 * 60 * 12
   };
 };
